a85ec856f6b311c2c3e36b22b202805028b4c043
Sagar chaurasia <57215331+sagarchaurasia260395@users.noreply.github.com>
2022-01-17 13:47:31 +0530

    Updated string length calculation

--- contracts/libraries/VanityNameServiceUtils.sol
+++ contracts/libraries/VanityNameServiceUtils.sol
@@ -37,7 +37,29 @@ library VanityNameServiceUtils {
      * NOTE : review the integrity of length using bytes converison.
      * bytes length does not always matches with UT8 length.
      */
-    function _getStrLen(string memory str) internal pure returns (uint256) {
-        return bytes(str).length;
+    // function _getStrLen(string memory str) internal pure returns (uint256) {
+    //     return bytes(str).length;
+    // }
+
+    /**
+     * Using assembly to get correct length of passed string
+     * Taken from : Stack Overflow
+     * https://ethereum.stackexchange.com/a/13886
+     */
+    function _getStrLen(string memory str) internal pure returns (uint256 length) {
+        uint256 i = 0;
+        bytes memory string_rep = bytes(str);
+
+        while (i < string_rep.length) {
+            if (string_rep[i] >> 7 == 0) i += 1;
+            else if (string_rep[i] >> 5 == bytes1(uint8(0x6))) i += 2;
+            else if (string_rep[i] >> 4 == bytes1(uint8(0xE))) i += 3;
+            else if (string_rep[i] >> 3 == bytes1(uint8(0x1E)))
+                i += 4;
+                //For safety
+            else i += 1;
+
+            length++;
+        }
     }
 }
